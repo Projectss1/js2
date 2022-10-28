db5c71a6330120d75a3485ef5831f51aa5d254e1
Projectss1 <114835870+Projectss1@users.noreply.github.com>
2022-10-28 14:54:21 +0300

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,167 @@
+// let age = prompt('Возраст?', );
+// let message = (age == 10) ? 'Верно':
+//  'Неверно';
+// alert(message);
+
+// let min = prompt("Введите число от 0 до 59");
+//       if (min >= 0 && min <= 14) {
+//         alert ("Первая часть");
+//      }
+//       if (min >= 15 && min <= 30) {
+//         alert ("Вторая часть");
+//      }45
+//      if (min >= 31 && min <= 45) {
+//         alert ("Третья часть");
+//      }
+//      if (min >= 46 && min <= 59) {
+//         alert ("Четвертая часть");
+//      }
+
+// let lang = prompt();
+// if (lang == 'ru') {
+// var arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+// };
+// if (lang == 'en') {
+// arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+// };
+// alert(arr);
+
+// Работа с if-else //
+
+// let a = prompt();
+// let message = (a == 0) ? 'Верно':
+//  'Неверно';
+// alert(message);
+
+// let a = prompt();
+// let message = (a > 0) ? 'Верно':
+//  'Неверно';
+// alert(message);
+
+// let a = prompt();
+// let message = (a < 0) ? 'Верно':
+//  'Неверно';
+// alert(message);
+
+// let a = prompt();
+// let message = (a >= 0) ? 'Верно':
+//  'Неверно';
+// alert(message);
+
+// let a = prompt();
+// let message = (a /= 0) ? 'Верно':
+//  'Неверно';
+// alert(message);
+
+// let a = prompt();
+// let message = (a == 'test') ? 'Верно':
+//  'Неверно';
+// alert(message);
+
+// let a = prompt();
+// let message = (a == '1' ) ? 'Верно':
+//  'Неверно';
+// alert(message);
+
+//  Работа с && (и) и || (или) //
+
+// let a = prompt();
+// let message = (a > 0 && a < 5) ? 'Верно':
+//  'Неверно';
+// alert(message);
+
+// let a = prompt();
+// let b = prompt();
+// let message = (a > 2 && a < 11) || (b > 6 && b < 14) ? 'Верно':
+//  'Неверно';
+// alert(message);
+
+// let  a = prompt();
+// if ( a == 0 || a == 2 ){
+//     a=+7
+//     alert(a);
+// }
+// else{
+//     a/=10
+//     alert(a);
+// }
+
+// Задачи на switch-case //
+
+// var num = +prompt('Введите число от 1 до 4',' ');
+
+// switch (num) {
+//   case 0:
+//     result = 'весна';
+//     break;
+
+//   case 1:
+//     result = 'лето';
+//     break;
+
+//   case 2:
+//     result = 'осень';
+//     break;
+//   case 3:
+//     result = 'зима';
+//     break;
+// }
+//   alert( result );
+
+//   Задачи //
+
+// let min = prompt("Введите число от 0 до 31");
+//       if (min >= 0 && min <= 10) {
+//         alert ("Первая часть");
+//      }
+//       if (min >= 10 && min <= 20) {
+//         alert ("Вторая часть");
+//      }45
+//      if (min >= 20 && min <= 32) {
+//         alert ("Третья часть");
+//      }
+
+// let min = prompt("Введите число от 1 до 12");
+//       if (min  >= 11  && min <= 13 ) {
+//         alert ("Зима");
+//      }
+//       if (min  >= 5 && min <= 9 ) {
+//         alert ("Лето");
+//      }45
+//      if (min  >=  2 && min <= 6 ) {
+//         alert ("Весна");
+//      }
+//      if (min  >= 8 && min <= 12 ) {
+//         alert ("Осень");
+//      }
+
+//  let str = 'abcde';
+//  if (str[0] == 'a'){
+//      alert('да')
+//  }
+//  else{
+//      alert('нет')
+//  }
+
+//  let str = '12345';
+//  if (str[0] == '1' || str[0] == '2' || str[0] == '3'){
+//      alert('да')
+//  }
+//  else{
+//      alert('нет')
+//  }
+
+// let str = '123';
+//  alert(Number(str[0]) + Number(str[1]) + Number(str[2]))
+//                                                         
+
+// let str = '123321';
+//  one = Number(str[0]) + Number(str[1]) + Number(str[2])
+//  two = Number(str[3]) + Number(str[4]) + Number(str[5])
+//  if (one == two){
+//      alert('да')
+//  }
+//  else{
+//      alert('нет')
+//  }
+
